251d010eaa7c593f26223515c579ce0b9dcffce1
LandisClark22 <landisclark72@gmail.com>
2026-04-02 19:04:15 +0000

    Completed Lab Data 2

--- src/main/java/CerealRunner.java
+++ src/main/java/CerealRunner.java
@@ -1,62 +1,60 @@
-/**************************
-* CerealRunner.java Starter Code
-* Note: You will need to complete all questions 
-* before you can run this class. If you wish to test one part at a time,
-* please comment out the unfinished methods and the related tests 
-* 
-************/
-
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.File;
 import java.io.FileNotFoundException; 
 import java.io.FileReader;
 
 public class CerealRunner
 {
-/* Question 1: Write filterCarbsPerCup
-   * This static method will return an ArrayList of cereal 
-   * objects with carbs per cup between min and max inclusive
-   * @param: min - the minimum integer value of the range
-   * @param: max - the maximum integer value of the range
-   * Precondition: min < max
-   */
-   public static  ArrayList<Cereal> filterCarbsPerCup(int min, int max)
+   /* Question 1: Write filterCarbsPerCup */
+   public static ArrayList<Cereal> filterCarbsPerCup(int min, int max)
    {
-      //Add your solution to Question 1 here.
+      ArrayList<Cereal> result = new ArrayList<Cereal>();
+
+      for (Cereal c : cereals)
+      {
+         double carbsPerCup = c.getCarbs() / c.getCups();
+
+         if (carbsPerCup >= min && carbsPerCup <= max)
+         {
+            result.add(c);
+         }
+      }
+
+      return result;
    }
    
-   /* Question 2: Write highestPercentFiber
-   * This static method will return the cereal with the highest 
-   * percentage of Fiber per calorie
-   * Precondition: cereals.size() > 0
-   */
-    
+   /* Question 2: Write highestPercentFiber */
    public static Cereal highestPercentFiber()
    {
-      //Add your solution to Question 2 here.
+      Cereal best = cereals.get(0);
+
+      for (Cereal c : cereals)
+      {
+         double currentRatio = c.getFiber() / c.getCalories();
+         double bestRatio = best.getFiber() / best.getCalories();
+
+         if (currentRatio > bestRatio)
+         {
+            best = c;
+         }
+      }
+
+      return best;
    }
   
-   
-   /* Questino 3: Write findNetCarbs
-   *  This static method will take in a cereal object and returns 
-   *  difference of carbs and fiber for that cereal
-   *  @param: c - Cereal object
-   */
-    
+   /* Question 3: Write findNetCarbs */
    public static double findNetCarbsPerCup(Cereal c)
    {
-      //Add your solution to Question 3 here.
+      // Match expected test output
+      return c.getCarbs() - c.getFiber();
    }
-  
 
    /*****************************************************************
     * The code below does not need to be edited.
     ****************************************************************/
    
-   //ArrayList of Cereal objects from cerealSubset.csv
    private static ArrayList<Cereal> cereals = new ArrayList<Cereal>(); 
-  
+   
    public CerealRunner(String fileName)
    {
       try 
@@ -73,21 +71,24 @@ public class CerealRunner
             double carbs = Double.parseDouble(myArray[3]); 
             double cups = Double.parseDouble(myArray[4]); 
             cereals.add(new Cereal(name, calories, fiber, carbs, cups));   
-         } //close while
-         scan.close();
-       } catch (FileNotFoundException e) 
-         {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
          }
+         scan.close();
+       } 
+       catch (FileNotFoundException e) 
+       {
+         System.out.println("An error occurred.");
+         e.printStackTrace();
+       }
+
       int numCereals = cereals.size();
-      System.out.println( numCereals + " records created.");   
+      System.out.println(numCereals + " records created.");   
    }
 
    public static void main(String [] args)
    {
       String fileName= "src/data/cerealSubset.csv";
-      CerealRunner cr = new CerealRunner(fileName);
+      new CerealRunner(fileName);
+
       ArrayList<Cereal> results = filterCarbsPerCup(17, 18);
       String names = "[";
       for(Cereal c : results)
@@ -97,8 +98,6 @@ public class CerealRunner
       else
          names += "]";
 
-       // These tests are not for grading! See src/test/java for those
-      //Test Question 1
       System.out.println("\n*****Filter Carbs Per Cup Results*****");
       System.out.println("Expected results: [Cinnamon Toast Crunch, Frosted "+
       "Mini-Wheats, Fruit & Fibre Dates; Walnuts; and Oats, Fruity Pebbles, "+
@@ -106,18 +105,13 @@ public class CerealRunner
       "Wheat, Wheaties]");
       System.out.println("\nActual results:   " + names);
 
-      //Test Part B
       System.out.println("\n*****Highest Percent Fiber Results*****");
       System.out.println("Expected results: All-Bran with Extra Fiber");
       System.out.println("Actual results:   " + highestPercentFiber().getName());
       
-
-      //Test Part C
       System.out.println("\n*****Find Net Carbs Results*****");
       Cereal testCereal = new Cereal("Golden Crisp",100,0,11,0.88);
       System.out.println("Expected results: 11.0");
       System.out.println("Actual results:   " + findNetCarbsPerCup(testCereal));
-      
    }
 }
-
